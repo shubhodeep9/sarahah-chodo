442d79ccf7d28852ca29dfbb014f212fb21991bb
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-08-22 23:55:01 +0530

    Ho toh gya, lets see

--- chodo.go
+++ chodo.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gopkg.in/headzoo/surf.v1"
+	"io/ioutil"
 	"net/url"
+	"runtime"
 	str "strings"
+	"sync"
 )
 
-func main() {
+var sent []string
+
+func sendMessage(username string) bool {
 	bow := surf.NewBrowser()
-	err := bow.Open("https://ambitionbox.sarahah.com/")
+	err := bow.Open("https://" + username + ".sarahah.com/")
 	if err != nil {
 		panic(err)
 	}
 
 	userid, _ := bow.Find("#RecipientId").Attr("value")
-	text := "Test from Go"
+	text := `Hiiii! How is your job going? Are you enjoying your work or finding it boring? Speak your heart out!
+The best part? You can do it anonymously. Go to Ambitionbox.cοm and rate your company now!`
 	scriptCode := bow.Find("script").Text()
 	startI := str.Index(scriptCode, `type="hidden" value="`)
 	endI := str.Index(scriptCode, `" />').attr('value')`)
@@ -25,7 +32,81 @@ func main() {
 	v.Add("text", text)
 	v.Add("captchaResponse", "")
 	v.Add("__RequestVerificationToken", requestVerification)
-	bow.PostForm("https://ambitionbox.sarahah.com/Messages/SendMessage", v)
-	fmt.Println(bow.Body())
+	bow.PostForm("https://"+username+".sarahah.com/Messages/SendMessage", v)
+	return bow.Body() == "&#34;Done&#34;"
+}
 
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var filewait sync.WaitGroup
+	filewait.Add(3)
+	go func() {
+		defer filewait.Done()
+		raw, err := ioutil.ReadFile("./unames123.json")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var names []string
+		json.Unmarshal(raw, &names)
+
+		var message123 sync.WaitGroup
+		message123.Add(len(names))
+		for _, name := range names {
+			go func() {
+				defer message123.Done()
+				if sendMessage(name) {
+					sent = append(sent, name)
+				}
+			}()
+		}
+		message123.Wait()
+	}()
+	go func() {
+		defer filewait.Done()
+		raw, err := ioutil.ReadFile("./unames.json")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var names []string
+		json.Unmarshal(raw, &names)
+
+		var message sync.WaitGroup
+		message.Add(len(names))
+		for _, name := range names {
+			go func() {
+				defer message.Done()
+				if sendMessage(name) {
+					sent = append(sent, name)
+				}
+			}()
+		}
+		message.Wait()
+	}()
+	go func() {
+		defer filewait.Done()
+		raw, err := ioutil.ReadFile("./ufirstnames.json")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var names []string
+		json.Unmarshal(raw, &names)
+
+		var firstmessage sync.WaitGroup
+		firstmessage.Add(len(names))
+		for _, name := range names {
+			go func() {
+				defer firstmessage.Done()
+				if sendMessage(name) {
+					sent = append(sent, name)
+				}
+			}()
+		}
+		firstmessage.Wait()
+	}()
+	filewait.Wait()
+	sentJson, _ := json.Marshal(sent)
+	ioutil.WriteFile("sent.json", sentJson, 0644)
 }
